a4e11f41b742ef2db19046aaf336f1ff94a77c23
Manuel Javier <manueljavierfc02032003@gmail.com>
2023-07-22 22:46:14 -0600

    Se agrega routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting = "Bienvenido"/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <ItemListContainer greeting="Bienvenido" /> } />
+          <Route path='/explore' element={ <ItemListContainer /> } />
+          {/* <Route path='/item/:idItem' element={ <ItemDetailContainer/> } /> */}
+        </Routes>
+      </BrowserRouter>
+      
+      {/* <ItemListContainer greeting = "Bienvenido"/> */}
       
     </div>
   );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./NavBar.css"
+/* import "./NavBar.css"; */
 import CartWidget from "../CartWidget/CartWidget.jsx";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     const logoImage = "https://cdn-icons-png.flaticon.com/512/1451/1451303.png"
@@ -16,12 +17,11 @@ function NavBar() {
         <div className="navContainer">
           
           <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Collection</a></li>
-            <li><a href="">Community</a></li>
-            <li><a href="">Profile</a></li>
-            <li><a href="">Explore</a></li>
-            <li><a href=""></a></li>
+            <li><Link to={"/"}>Home</Link></li>
+            <li><Link to={"/collection"}>Collection</Link></li>
+            <li><Link to={"/community"}>Community</Link></li>
+            <li><Link to={"/profile"}>Profile</Link></li>
+            <li><Link to={"/explore"}>Explore</Link></li>
           </ul>
 
           <CartWidget/>
